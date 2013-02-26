f7771bae1c08c2dad219bf2acac78c0ba6a854cb
kiemkhachdt <kiemkhachdt@gmail.com>
2013-02-26 16:58:19 +0700

    Thêm nút Thoát và viết hàm thoát

--- Form1.cs
+++ Form1.cs
@@ -14,5 +14,10 @@ namespace exampleproject_hieu
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
